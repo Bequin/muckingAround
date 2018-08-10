8c0622c7515fd5a63fd48f7c0b35a980168fbc9d
digkuchler <digkuchler@W-000417>
2018-08-10 15:33:42 +0100

    Add project files.

---
+++ helloLads/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace helloLads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var Name = TryAnswer();
+
+            Console.WriteLine("What's your age?");
+            var Age = TryAnswer();
+
+            Console.WriteLine("What month were you born?");
+            var Month = TryAnswer();
+
+            Console.WriteLine("Name: " + Name + ", age: " + Age + ", Month: " + Month);
+
+            if (Month == "March")
+            {
+                Console.WriteLine("You're an Aeries.");
+            }
+
+        }
+
+        static string TryAnswer()
+        {
+            var Answer = Console.ReadLine();
+
+            if (Answer == "")
+            {
+                Console.WriteLine("You didn't write anything, champ. Please try again.");
+                return Console.ReadLine();
+            }
+                return Answer;
+        }
+
+    }
+}
